fe99e403763a86fb5796a29e50f350867f1b60d8
ryandagg <ryan.dagg@gmail.com>
2014-07-08 15:58:21 -0600

    filter by author working, but needs a reset button on page.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var quoteSpace = (function(){
+var QuoteSpace = (function(){
 
 	var quotes = [];
 
@@ -13,57 +13,62 @@ var quoteSpace = (function(){
 	};
 
 	// sorts the quotes by rating
-	var sortQuotes = function() {
-		quotes = _.sortBy(quotes, function(obj) {
+	var sortByRating = function(array) {
+		return _.sortBy(array, function(obj) {
 			return obj.rating;
 		}).reverse();
 	};
 
-	var populateQuotes = function(div) {
-		sortQuotes();
+	var populateQuotes = function(div, array) {
+		var newArray = sortByRating(array);
 		$(div).empty();
-		for (var i = 0; i < quotes.length; i++) {
+		for (var i = 0; i < newArray.length; i++) {
 			$(div).append(
 				"<div class = 'quote-block'>" + 
-					"<p class = 'a-quote'>" + quotes[i].text + "</p>" + 
-					"<p class = 'an-author'>" + quotes[i].author + "</p>" +
+					"<p class = 'a-quote'>" + newArray[i].text + "</p>" + 
+					"<a class = 'an-author'>" + newArray[i].author + "</a>" +
+					"<div class = 'a-rating'>" + newArray[i].rating + " stars</div>" +
 				"</div>"
 			)
 		}
+	};
+
+	var filterQuotes = function(array, author) {
+		return array.filter(function(obj) {
+			return obj.author === author;
+		})
 	};
 
 	return {
 		quotes: quotes,
 		addQuote: addQuote,
 		populateQuotes: populateQuotes,
+		filterQuotes: filterQuotes,
 	};
 
 
 })();
 
-quoteSpace.addQuote("The Edge... there is no honest way to explain it because the only people who really know where it is are the ones who have gone over.", "Hunter S. Thompson", 5)
-quoteSpace.addQuote("America... just a nation of two hundred million used car salesmen with all the money we need to buy guns and no qualms about killing anybody else in the world who tries to make us uncomfortable.", "Hunter S. Thompson", 1)
-quoteSpace.addQuote("In a closed society where everybody's guilty, the only crime is getting caught. In a world of thieves, the only final sin is stupidity.", "Hunter S. Thompson", 4)
-quoteSpace.addQuote("For every moment of triumph, for every instance of beauty, many souls must be trampled.", "Hunter S. Thompson", 5)
+QuoteSpace.addQuote("The Edge... there is no honest way to explain it because the only people who really know where it is are the ones who have gone over.", "Hunter S. Thompson", 5)
+QuoteSpace.addQuote("America... just a nation of two hundred million used car salesmen with all the money we need to buy guns and no qualms about killing anybody else in the world who tries to make us uncomfortable.", "Hunter S. Thompson", 1)
+QuoteSpace.addQuote("In a closed society where everybody's guilty, the only crime is getting caught. In a world of thieves, the only final sin is stupidity.", "Hunter S. Thompson", 4)
+QuoteSpace.addQuote("For every moment of triumph, for every instance of beauty, many souls must be trampled.", "Hunter S. Thompson", 3)
+QuoteSpace.addQuote("Don’t ask what the world needs. Ask what makes you come alive, and go do it. Because what the world needs is people who have come alive.", "Howard Thurman", 5)
 
-// console.log(quoteSpace.quotes)
+// console.log(QuoteSpace.quotes)
 
 
 $(document).on('ready', function() {
-	quoteSpace.populateQuotes(".quotes-main");
-	// replace this with a function
-	// for (var i = 0; i < quoteSpace.quotes.length; i++) {
-	// 	$(".quotes-main").append(
-	// 		"<div class = 'quote-block'>" + 
-	// 		"<p class = 'a-quote'>" + quoteSpace.quotes[i].text + "</p>" + 
-	// 		"<p class = 'an-author'>" + quoteSpace.quotes[i].author + "</p>" +
-	// 		"</div>"
-	// 	)
-	// }
+	QuoteSpace.populateQuotes(".quotes-main", QuoteSpace.quotes);
 
+	// quote submit button handler
 	$(document).on("click", ".submit-button", function() {
-		// THIS IS NOT WORKING
-		quoteSpace.addQuote($(".quote-text-input").val(), $(".author-text-input").val(), $('input:radio[name=rating]:checked').val());
-		quoteSpace.populateQuotes(".quotes-main");
+		// submit button functionality
+		QuoteSpace.addQuote($(".quote-text-input").val(), $(".author-text-input").val(), $('input:radio[name=rating]:checked').val());
+		QuoteSpace.populateQuotes(".quotes-main", QuoteSpace.quotes);
 	})
+	$(document).on("click", ".an-author", function() {
+		QuoteSpace.populateQuotes(".quotes-main", QuoteSpace.filterQuotes(QuoteSpace.quotes, $(this).text()))
+	})
+
 });
